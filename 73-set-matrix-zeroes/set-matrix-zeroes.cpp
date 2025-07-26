3e57a5ae07f036f4fc5c1e516c311a3e186f76d5
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-07-26 20:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,49 +1,92 @@
 class Solution {
 public:
 
-    void setRow(vector<vector<int>>& matrix,int n ,int m, int i)
-    {
-        int j;
-        for(j=0;j<m;++j)
-        {
-            if(matrix[i][j]!=0)
-            {
-                matrix[i][j] =-1212;
-            }
-        }
-    }
-    void setCol(vector<vector<int>>& matrix,int n ,int m, int j)
-    {
-        int i;
-        for(i=0;i<n;++i)
-        {
-            if(matrix[i][j]!=0)
-            {
-                matrix[i][j] =-1212;
-            }
-        }
-    }
+    // void setRow(vector<vector<int>>& matrix,int n ,int m, int i)
+    // {
+    //     int j;
+    //     for(j=0;j<m;++j)
+    //     {
+    //         if(matrix[i][j]!=0)
+    //         {
+    //             matrix[i][j] =-1212;
+    //         }
+    //     }
+    // }
+    // void setCol(vector<vector<int>>& matrix,int n ,int m, int j)
+    // {
+    //     int i;
+    //     for(i=0;i<n;++i)
+    //     {
+    //         if(matrix[i][j]!=0)
+    //         {
+    //             matrix[i][j] =-1212;
+    //         }
+    //     }
+    // }
 
+    // void setZeroes(vector<vector<int>>& matrix) {
+    //     int i,j;
+    //     int n=matrix.size();
+    //     int m=matrix[0].size();
+    //     for(i=0;i<n;++i)
+    //     {
+    //         for(j=0;j<m;++j)
+    //         {
+    //             if(matrix[i][j] == 0)
+    //             {
+    //                 setRow(matrix,n,m,i);
+    //                 setCol(matrix,n,m,j);
+    //             }
+    //         }
+    //     }
+    //     for(i=0;i<n;++i)
+    //     {
+    //         for(j=0;j<m;++j)
+    //         {
+    //             if(matrix[i][j]==-1212)
+    //             {
+    //                 matrix[i][j] =0;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // // better approach 
     void setZeroes(vector<vector<int>>& matrix) {
         int i,j;
         int n=matrix.size();
         int m=matrix[0].size();
+        vector<int>k(m,0);
+        vector<int>p(n,0);
         for(i=0;i<n;++i)
         {
             for(j=0;j<m;++j)
             {
                 if(matrix[i][j] == 0)
                 {
-                    setRow(matrix,n,m,i);
-                    setCol(matrix,n,m,j);
+                    k[j]=1;
+                    p[i]=1;
                 }
             }
         }
-        for(i=0;i<n;++i)
+        for(int s=0;s<m;++s)
         {
-            for(j=0;j<m;++j)
+            if(k[s]==1)
             {
-                if(matrix[i][j]==-1212)
+                j=s;
+                for(i=0;i<n;++i)
+                {
+                    matrix[i][j] =0;
+                }
+            }
+        }
+
+        for(int r = 0;r<n;++r)
+        {
+            if(p[r] == 1)
+            {
+                i=r;
+                for(j=0;j<m;++j)
                 {
                     matrix[i][j] =0;
                 }
