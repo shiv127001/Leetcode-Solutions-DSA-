3c0a4366f4ea3d8c7a7826aa0b7f81287836b9d7
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-07-29 23:11:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.49%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,47 +1,62 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int p,q,i=0,j=0;
+        // Method 2 using 2 pointer method
         if(n==0){
             return;
         }
+        int q=0;
         if(m==0){
-            for(i=0;i<n;++i)
+            for(int ii=0;ii<n;++ii)
             {
-                nums1[i] = nums2[j];
-                j++;
+                nums1[ii] = nums2[q];
+                q++;
             }
             return ;
         }
 
-        int k=0;
-        for(i=m;i<(m+n);++i)
+        int i=m-1,j=n-1,k=(m+n)-1;
+        while(i>=0 && j>=0)
         {
-            nums1[i] = nums2[k];
-            k++;
+            if(nums1[i]>=nums2[j]) {
+                nums1[k] = nums1[i];
+                k--;i--;
+            }
+            else if(nums1[i]<nums2[j])
+            {
+                nums1[k] = nums2[j];
+                k--;j--;
+            }
         }
-        sort(nums1.begin(),nums1.end());
+        // // if nums2 still has elements left (nums1 is already in place)
+        while(j>=0)
+        {
+            nums1[k] = nums2[j];
+            k--;j--;
+        }
 
 
 
-        // while(i<m || j<n)
-        // {
-        //     if(nums1[i]<=nums2[j]){
-        //         i++;
-        //     }
-        //     if(nums1[i]>nums2[j])
-        //     {
-        //         swap(nums1[i],nums2[j]);
-        //         j++;i++;
-        //     }
+        // // Method 1 
+        // int p,q,i=0,j=0;
+        // if(n==0){
+        //     return;
         // }
-        // sort(nums2.begin(),nums2.end());
+        // if(m==0){
+        //     for(i=0;i<n;++i)
+        //     {
+        //         nums1[i] = nums2[j];
+        //         j++;
+        //     }
+        //     return ;
+        // }
+
         // int k=0;
         // for(i=m;i<(m+n);++i)
         // {
         //     nums1[i] = nums2[k];
         //     k++;
         // }
-
+        // sort(nums1.begin(),nums1.end());
     }
 };
