74cfffd9b9af86069b46afcf2b0786b7858b6b93
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-07-31 10:43:05 +0530

    Time: 6 ms (12.39%) | Memory: 28.1 MB (86.44%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> mpp;
+        int n = nums.size();
+        for(int i=0;i<n;++i)
+        {
+            mpp[nums[i]] += 1;
+        }
+        for(auto it: mpp)
+        {
+            if(it.second > ceil(n/2))
+            {
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
