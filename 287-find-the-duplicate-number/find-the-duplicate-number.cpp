f2a83adfc823f525d54e71dfc0c62a6d56d5a3d3
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-08-16 13:07:42 +0530

    Time: 23 ms (40.51%) | Memory: 65.1 MB (42.82%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        map<int,int> mpp;
-        int n = nums.size();
-        for(int i=0;i<n;++i)
-        {
-            mpp[nums[i]]++;
+        
+        // // optimal
+
+        // // better
+        int i,cnt,n=nums.size(), result;
+        int low = 1, high = n - 1, duplicate = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // Count numbers <= mid
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) count++;
+            }
+
+            if (count > mid) {
+                // Duplicate is in [low, mid]
+                duplicate = mid;
+                high = mid - 1;
+            } else {
+                // Duplicate is in [mid+1, high]
+                low = mid + 1;
+            }
         }
-        int m = mpp.size(),a;
-        for(auto it:mpp)
-        {
-            if(it.second>1) a = it.first;
-        }
-        return a;
+        return duplicate;
+
+
+
+        // // brute force
+        // map<int,int> mpp;
+        // int n = nums.size();
+        // for(int i=0;i<n;++i)
+        // {
+        //     mpp[nums[i]]++;
+        // }
+        // int m = mpp.size(),a;
+        // for(auto it:mpp)
+        // {
+        //     if(it.second>1) a = it.first;
+        // }
+        // return a;
     }
 };
