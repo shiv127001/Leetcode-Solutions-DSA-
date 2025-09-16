b121bf1b296ec9313761849a3b4d255f9c9ba0d7
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-09-16 18:53:26 +0530

    Time: 181 ms (5.36%) | Memory: 67.2 MB (16.10%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -18,27 +18,27 @@ public:
         }
         return cnt;
     }
-    int upto_k_minus_1(vector<int>& nums, int k)
-    {
-        int l = 0, r = 0, n = nums.size(), cnt = 0;
-        map<int, int> mpp;
-        while (r < n)
-        {
-            mpp[nums[r]]++;
-            while((int)mpp.size() > k)
-            {
-                mpp[nums[l]]--;
-                if (mpp[nums[l]] == 0) mpp.erase(nums[l]);
-                l++;
-            }
-            cnt+=r-l+1;
-            r++;
-        }
-        return cnt;
-    }
+    // int upto_k_minus_1(vector<int>& nums, int k)
+    // {
+    //     int l = 0, r = 0, n = nums.size(), cnt = 0;
+    //     map<int, int> mpp;
+    //     while (r < n)
+    //     {
+    //         mpp[nums[r]]++;
+    //         while((int)mpp.size() > k)
+    //         {
+    //             mpp[nums[l]]--;
+    //             if (mpp[nums[l]] == 0) mpp.erase(nums[l]);
+    //             l++;
+    //         }
+    //         cnt+=r-l+1;
+    //         r++;
+    //     }
+    //     return cnt;
+    // }
     int subarraysWithKDistinct(vector<int>& nums, int k) 
     {
-        int ans = upto_k(nums,k) - upto_k_minus_1(nums,k-1);
+        int ans = upto_k(nums,k) - upto_k(nums,k-1);
         return ans;
 
     }
