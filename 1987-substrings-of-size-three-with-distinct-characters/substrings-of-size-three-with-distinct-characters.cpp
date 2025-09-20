d342791ebc67c8bbe52b7f755321f40e97adf909
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-09-20 23:08:58 +0530

    Time: 3 ms (26.87%) | Memory: 9 MB (22.18%) - LeetSync

---
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uptok(string s,int k)
+    {
+        map<char,int>mpp;
+        int l=0,r=0,cnt=0, n=s.size();
+        while(r<n)
+        {
+            mpp[s[r]]++;
+            if(r - l + 1 > 3)
+            {
+                mpp[s[l]]--;
+                if(mpp[s[l]]==0) mpp.erase(s[l]);
+                l++;
+            }
+            if(r - l + 1 == 3 && mpp.size() == 3) cnt++;
+            r++;
+        }
+        return cnt;
+
+    }
+    int countGoodSubstrings(string s) {
+        int k=3;
+        int n=s.size();
+        if(n < 3) return 0;
+        return (uptok(s,k));
+    }
+};
