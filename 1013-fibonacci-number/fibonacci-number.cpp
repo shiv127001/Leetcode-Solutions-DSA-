c66044910728a7fccaaa5ab20dc9e31c02cc8484
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-09-22 12:28:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (19.58%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int fib_DP(int n,vector<int>& dp)
+    int fib_DP_memoization(int n,vector<int>& dp)
     {
         if(n<=1) return n;
         if(dp[n]!=-1) return dp[n];
-        dp[n] = fib_DP(n-1,dp)+fib_DP(n-2,dp);
+        dp[n] = fib_DP_memoization(n-1,dp)+fib_DP_memoization(n-2,dp);
         return dp[n];
 
     }
     int fib(int n) {
         vector<int> dp(n+1,-1);
-        return fib_DP(n,dp);
+        return fib_DP_memoization(n,dp);
     }
 };
