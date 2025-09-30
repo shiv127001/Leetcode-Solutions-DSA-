ac4077a1eb8b7990f32b85197f5dbb76a59c4329
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-09-30 16:38:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (77.73%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n <= 0) return false;
         if (n==0) return false;
         if (n==-1) return false;
-        // if (n==1) return true;
-        // if (n%3!=0) return false;
-        // return isPowerOfThree(n/3);
-        double result = log(n)/log(3);
-        int nearest = round(result);
-        return fabs(result - nearest) < 1e-10; // Use a tolerance check instead of exact equality:
+        if (n==1) return true;
+        if (n%3!=0) return false;
+        return isPowerOfThree(n/3);
+
+        // if (n <= 0) return false;
+        // if (n==0) return false;
+        // if (n==-1) return false;
+        // double result = log(n)/log(3);
+        // int nearest = round(result);
+        // return fabs(result - nearest) < 1e-10; // Use a tolerance check instead of exact equality:
 
     }
 };
