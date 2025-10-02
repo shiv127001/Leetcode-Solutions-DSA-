593f5d0c12b31d64bb33c012a838cf0304285971
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-10-02 11:47:10 +0530

    Time: 75 ms (7.85%) | Memory: 64.3 MB (5.07%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -1,31 +1,57 @@
 class Solution {
 public:
-    int totalNumbers(vector<int>& digits) {
-        int n=digits.size(),cnt=0,j,k;
+    int totalNumbers(vector<int>& digits) 
+    {
+        int n=digits.size();
         set<int> result;
-        for(int i=0;i<n;++i)
+        vector<int> freq(10,0);
+        for (int d : digits) freq[d]++;
+        for(int i=100;i<=999;++i)
         {
-            if(digits[i] %2!=0) cnt++;
-        }
-        if(cnt==n) return 0;
-        map<int,int> mpp;
-        for(int i=0;i<n;++i)
-        {
-            for(j=0;j<n;++j)
+            if(i%2!=0) continue;
+            int num = i;
+            vector<int> need(10,0);
+            while(num>0)
             {
-                if(i==j) continue;
-                for(k=0;k<n;++k)
-                {
-                    if(k==i || k==j) continue;
-                    int d1 = digits[i];
-                    int d2 = digits[j];
-                    int d3 = digits[k];
-                    if(d1==0 || d3%2!=0) continue;
-                    int dig = 100*d1+10*d2+d3;
-                    result.insert(dig);
-                }
+                need[num%10]++;
+                num=num/10;
             }
+            bool ans =true;
+            for(int d=0;d<10;++d)
+            {
+                if(need[d] > freq[d]) ans = false;
+            }
+            if(ans) result.insert(i);
         }
         return result.size();
     }
+
+    // {
+    //     int n=digits.size(),cnt=0,j,k;
+    //     set<int> result;
+    //     for(int i=0;i<n;++i)
+    //     {
+    //         if(digits[i] %2!=0) cnt++;
+    //     }
+    //     if(cnt==n) return 0;
+    //     map<int,int> mpp;
+    //     for(int i=0;i<n;++i)
+    //     {
+    //         for(j=0;j<n;++j)
+    //         {
+    //             if(i==j) continue;
+    //             for(k=0;k<n;++k)
+    //             {
+    //                 if(k==i || k==j) continue;
+    //                 int d1 = digits[i];
+    //                 int d2 = digits[j];
+    //                 int d3 = digits[k];
+    //                 if(d1==0 || d3%2!=0) continue;
+    //                 int dig = 100*d1+10*d2+d3;
+    //                 result.insert(dig);
+    //             }
+    //         }
+    //     }
+    //     return result.size();
+    // }
 };
