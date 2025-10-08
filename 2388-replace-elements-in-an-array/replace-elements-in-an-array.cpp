f2137c1d7cca9a5c275d40294465a720739dd526
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-10-08 09:18:17 +0530

    Time: 105 ms (54.31%) | Memory: 134.2 MB (65.30%) - LeetSync

--- 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
@@ -6,17 +6,21 @@ public:
         for(int i=0;i<n;++i) mpp[nums[i]] = i;
         for(int j=0;j<m;++j)
         {
-            // int oldval = operations[j][0];
-            // int newval = operations[j][1];
+            int oldval = operations[j][0];
+            int newval = operations[j][1];
+            int indx = mpp[oldval];
+            nums[indx] = newval;
+            mpp.erase(oldval);
+            mpp[newval] = indx;
 
 
-            if(mpp.find(operations[j][0])!=mpp.end())
-            {
-                auto it = mpp.find(operations[j][0]);
-                nums[it->second] = operations[j][1];
-                mpp[operations[j][1]] = it->second;
-                mpp.erase(operations[j][0]);
-            }
+            // if(mpp.find(operations[j][0])!=mpp.end())
+            // {
+            //     auto it = mpp.find(operations[j][0]);
+            //     nums[it->second] = operations[j][1];
+            //     mpp[operations[j][1]] = it->second;
+            //     mpp.erase(operations[j][0]);
+            // }
         }
         return nums;
     }
