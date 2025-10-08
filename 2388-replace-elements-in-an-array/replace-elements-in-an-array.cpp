2d513c67410e34f7a62156ea39d4b400853ca903
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-10-08 09:14:08 +0530

    Time: 111 ms (43.53%) | Memory: 134.2 MB (65.30%) - LeetSync

---
+++ 2388-replace-elements-in-an-array/replace-elements-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
+        unordered_map<int,int> mpp;
+        int n=nums.size(), m=operations.size();
+        for(int i=0;i<n;++i) mpp[nums[i]] = i;
+        for(int j=0;j<m;++j)
+        {
+            // int oldval = operations[j][0];
+            // int newval = operations[j][1];
+
+
+            if(mpp.find(operations[j][0])!=mpp.end())
+            {
+                auto it = mpp.find(operations[j][0]);
+                nums[it->second] = operations[j][1];
+                mpp[operations[j][1]] = it->second;
+                mpp.erase(operations[j][0]);
+            }
+        }
+        return nums;
+    }
+};
