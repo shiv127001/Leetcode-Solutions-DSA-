2a76e2d38e609296dcea428fafbceab3ee47c3d9
Shivank <123369060+shiv127001@users.noreply.github.com>
2025-10-13 12:26:11 +0530

    Time: 24 ms (20.89%) | Memory: 31.3 MB (11.64%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int i,j,m = mat.size(),n = mat[0].size();
+        map<int,vector<int>> mpp;
+        for(i=0;i<m;++i)
+        {
+            for(j=0;j<n;++j)
+            {
+                mpp[i+j].push_back(mat[i][j]);
+            }
+        }
+        vector<int>res;
+        for(int k=0;k<mpp.size();++k)
+        {
+            if(k%2==0) {
+                reverse(mpp[k].begin(),mpp[k].end());
+            }
+            for(int val: mpp[k]) {
+                res.push_back(val);
+            }
+        }
+        return res;
+    }
+};
